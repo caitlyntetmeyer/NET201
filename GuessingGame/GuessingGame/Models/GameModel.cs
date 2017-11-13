4e15e15f9b65d89c2d11e53774db2fe6c5dde286
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-11-13 15:03:09 -0500

    1st version of guessing game complete

---
+++ GuessingGame/GuessingGame/Models/GameModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class GameModel
+    {
+        public string PlayerName { get; set; }
+
+        public int Guess { get; set; }
+    }
+}
