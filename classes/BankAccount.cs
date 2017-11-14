4b9a29815f9b2d5f98c2ce11e8c7213991831ac8
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-11-14 13:24:56 -0500

    created deposits and withdrawals

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -1,14 +1,24 @@
+using System.Collections.Generic;
 using System;
 
 namespace classes
 {
     public class BankAccount    // This line defines the class, or type, we're creating.
     {   /* Everything inside the {} that follows the class declaration defines the BEHAVIOR of the class. There are 5 MEMBERS of the BankAccount class. 
-        The first 3 are PROPERTIES. */
+        The first 3 are PROPERTIES. Properties are DATA ELEMENTS and can have code that enforces validation or other rules.*/
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
-        // Properties are DATA ELEMENTS and can have code that enforces validation or other rules.
+        public decimal Balance 
+        { 
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                    balance += item.Amount;
+                return balance;
+            }
+
+        }
 
 
         // These 2 members are METHODS. Methods are blocks of code that perform a single function.
@@ -31,7 +41,10 @@ namespace classes
             accountNumberSeed++;
         }
 
-        // This is a data member. It's private, so it can only be accessed by code inside the BankAccount.
+        // This is a data member. It's PRIVATE, so it can only be accessed by code INSIDE the BankAccount.
         private static int accountNumberSeed = 1234567890;
+
+        // This declaration adds a List<T> of Transaction objects to the BankAccount class:
+        private List<Transaction> allTransactions = new List<Transaction>();
     }
 }
